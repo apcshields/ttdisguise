5367c088a59062b7ff32b2e7067c8a69dbc8b8da
Andrew Shields <apcshields@gmail.com>
2011-11-12 14:50:05 -0500

    Hmmm...

--- ttdiguise.user.js
+++ ttdiguise.user.js
@@ -8,8 +8,8 @@
 
 (function() {
 	var scripts = [
-	   // 'https://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js',
-	    'https://raw.github.com/apcshields/ttdisguise/master/engine.js'
+	   //'https://raw.github.com/apcshields/ttdisguise/master/engine.js'
+	   'http://brothersjunk.com/hack/ttdisguise/engine.js'
 	];
 	
 	for (i in scripts) {
