17f2697d3eae7c9c060971a7899e894628ccdea9
Andrew Shields <apcshields@gmail.com>
2011-11-11 22:01:17 -0500

    A test commit.

---
+++ engine.js
@@ -0,0 +1,3 @@
+if (Prototype) {
+	var test = new Element('p').setStyle({ position: 'absolute', margin: '3em auto', padding: '1em', fontSize: '48px', color: 'yellow', background: 'red' }).update('Hello!');
+}
---
+++ ttdiguise.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name                turntable disguise
+// @namespace	        https://github.com/apcshields/ttdisguise
+// @description	        Allows you to apply disguises to turntable members.
+// @include				http://turntable.fm/*
+// ==/UserScript==
+
+(function() {
+	var scripts = [
+	    'https://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js',
+	    'https://raw.github.com/apcshields/ttdisguise/engine.js'
+	];
+	
+	for (i in scripts) {
+	    var script = document.createElement('script');
+	    script.src = scripts[i];
+	    document.getElementsByTagName('body')[0].appendChild(script);
+	}
+})();
